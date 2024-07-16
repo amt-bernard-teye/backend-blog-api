8ddf200af9be5282d607655ea75acd32e424277b
Bernard Teye <bernard.teye@amalitech.com>
2024-07-16 08:48:21 +0000

    add database.module.ts and import into app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     JwtModule.register({global: true}),
-    ConfigModule.forRoot({isGlobal: true})
+    ConfigModule.forRoot({isGlobal: true}),
+    DatabaseModule
   ],
   controllers: [AppController],
   providers: [AppService],
